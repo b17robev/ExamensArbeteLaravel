9549f23b9a3a11716bb78fe895e6d467d91406be
b17robev <b17robev@student.his.se>
2020-04-01 18:39:35 +0200

    added propeties to model

--- app/Airport.php
+++ app/Airport.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airport extends Model
 {
-    //
+    protected $fillable = [
+        'airport_id',
+        'name', 'city', 'country', 'iata', 'icao',
+        'latitude', 'longitude', 'altitude', 'timezone', 'dst',
+        'tz_database_time_zone', 'type', 'source', 'location',
+    ];
+
+    protected $primaryKey = 'airport_id';
 }
