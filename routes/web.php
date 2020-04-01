b98ac6c14c63acba3acec1bb7067eefec04993dd
b17robev <b17robev@student.his.se>
2020-04-01 19:07:06 +0200

    add show route

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/airports', 'AirportsController@index');
 Route::post('/airports', 'AirportsController@store');
+Route::get('/airports/{id}', 'AirportsController@show');
 Route::patch('/airports/{id}', 'AirportsController@update');
 Route::delete('/airports/{id}', 'AirportsController@delete');
